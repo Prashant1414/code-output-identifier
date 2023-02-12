01610e5ede752ad05f405b5ab05662566ef6633e
Prashant1414 <Prashant1414@github.com>
2023-02-12 20:33:29 +0530

    initial commit

---
+++ src/main/java/app/com/ArrayDemo.java
@@ -0,0 +1,11 @@
+package app.com;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		
+		int testArray[] = {1,2,3,4,5};
+        System.out.println("testArray.length: "+testArray.length);
+	}
+
+}
