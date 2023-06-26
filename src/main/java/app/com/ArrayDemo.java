a70957ca0f548381dee9e17d2bf5bea844a4194d
pshir <pshir@host.docker.internal>
2023-06-27 00:45:15 +0530

    added output

--- src/main/java/app/com/ArrayDemo.java
+++ src/main/java/app/com/ArrayDemo.java
@@ -5,7 +5,7 @@ public class ArrayDemo {
 	public static void main(String[] args) {
 		
 		int testArray[] = {1,2,053,4,5};
-        System.out.println("testArray.length: "+testArray.length);
+        System.out.println("testArray.length: "+testArray.length); // output: testArray.length: 5
 	}
 
 }
