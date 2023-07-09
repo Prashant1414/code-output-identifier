ddadd0b9e60092bdb768be7a8b0c1f28b815b4c3
pshir <pshir@host.docker.internal>
2023-07-09 16:48:15 +0530

    added arraydemo

--- src/main/java/app/com/ArrayDemo.java
+++ src/main/java/app/com/ArrayDemo.java
@@ -1,11 +1,21 @@
 package app.com;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class ArrayDemo {
 
 	public static void main(String[] args) {
 		
 		int testArray[] = {1,2,053,4,5};
         System.out.println("testArray.length: "+testArray.length); // output: testArray.length: 5
+        
+        AtomicLong idGenerator = new AtomicLong();
+        System.out.println(idGenerator.getAndIncrement());
+        System.out.println(idGenerator.getAndIncrement());
+        System.out.println(idGenerator.getAndIncrement());
+        System.out.println(idGenerator.getAndIncrement());
+        System.out.println(idGenerator.getAndIncrement());
+        System.out.println(idGenerator.getAndIncrement());
 	}
 
 }
